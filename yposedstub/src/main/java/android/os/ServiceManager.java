968bd48c52c310820a1eaf05aa34566b33f09754
jamesfchen <hawks.jamesf@gmail.com>
2021-03-15 16:18:03 +0800

    add android stub

--- yposedstub/src/main/java/android/os/ServiceManager.java
+++ yposedstub/src/main/java/android/os/ServiceManager.java
@@ -1,5 +1,7 @@
 package android.os;
 
+import java.util.Map;
+
 /**
  * Copyright ® $ 2017
  * All right reserved.
@@ -9,6 +11,25 @@ package android.os;
  */
 public class ServiceManager {
     public static IBinder getService(String name) {
+        throw new RuntimeException("Stub!");
+    }
+    public static void addService(final String name, final IBinder service) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public static void addService(final String name, final IBinder service, final boolean allowIsolated) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public static IBinder checkService(final String name) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public static String[] listServices() throws RemoteException {
+        throw new RuntimeException("Stub!");
+    }
+
+    public static void initServiceCache(final Map<String, IBinder> cache) {
         throw new RuntimeException("Stub!");
     }
 }
