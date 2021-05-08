bc5f1d5e74d75f50cd96b07852d84c2dce69c846
jamesfchen <hawks.jamesf@gmail.com>
2021-05-08 23:45:29 +0800

    update

--- yposedplugin3/src/main/java/com/jamesfchen/yposedplugin3/B.java
+++ yposedplugin3/src/main/java/com/jamesfchen/yposedplugin3/B.java
@@ -1,14 +1,14 @@
 package com.jamesfchen.yposedplugin3;
 
 import android.content.Context;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ViewConfiguration;
 import android.view.ViewParent;
 import android.widget.LinearLayout;
-import android.widget.OverScroller;
+
+import androidx.annotation.Nullable;
 
 /**
  * Copyright ® $ 2017
@@ -17,52 +17,43 @@ import android.widget.OverScroller;
  * @author: hawks.jamesf
  * @since: May/08/2021  Sat
  */
-public class MyViewGroup2 extends LinearLayout {
-    public MyViewGroup2(Context context) {
-        super(context);
-    }
-
-    public MyViewGroup2(Context context, AttributeSet attrs) {
+public class B extends LinearLayout {
+    public B(Context context, AttributeSet attrs) {
         super(context, attrs);
         initScrollView();
     }
 
-    public MyViewGroup2(Context context, AttributeSet attrs, int defStyleAttr) {
+    public B(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initScrollView();
-
     }
-    private OverScroller mScroller;
+
     private int mTouchSlop;
-    private int mMinimumVelocity;
-    private int mMaximumVelocity;
-    private int mOverscrollDistance;
-    private int mOverflingDistance;
-    private float mVerticalScrollFactor;
+
     private void initScrollView() {
-        mScroller = new OverScroller(getContext());
         setFocusable(true);
         setDescendantFocusability(FOCUS_AFTER_DESCENDANTS);
         setWillNotDraw(false);
         final ViewConfiguration configuration = ViewConfiguration.get(getContext());
         mTouchSlop = configuration.getScaledTouchSlop();
-        mMinimumVelocity = configuration.getScaledMinimumFlingVelocity();
-        mMaximumVelocity = configuration.getScaledMaximumFlingVelocity();
-        mOverscrollDistance = configuration.getScaledOverscrollDistance();
-        mOverflingDistance = configuration.getScaledOverflingDistance();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            mVerticalScrollFactor = configuration.getScaledVerticalScrollFactor();
-        }
     }
 
     private boolean mIsBeingDragged = false;
     private float mLastMotionY;
     private int mActivePointerId;
-//    private VelocityTracker mVelocityTracker;
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        Log.d("cjf", "B#onInterceptTouchEvent + " + action);
+        return super.dispatchTouchEvent(ev);
+    }
+
+    //外部拦截
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         int action = ev.getAction();
-        Log.w("cjf", "MyViewGroup2#onInterceptTouchEvent + "+action);
+        Log.d("cjf", "B#onInterceptTouchEvent + " + action);
 //        return super.onInterceptTouchEvent(ev);
         //在捕获了滑动事件，通过onInterceptTouchEvent返回ture拦截不向下传递，通过requestDisallowInterceptTouchEvent不让父节点拦截，这样就能只能自己处理滑动事件，不受其他干扰
         if (action == MotionEvent.ACTION_DOWN) {
@@ -70,9 +61,9 @@ public class MyViewGroup2 extends LinearLayout {
             mLastMotionY = y;
             mActivePointerId = ev.getPointerId(0);
 //            mVelocityTracker.addMovement(ev);
-            mIsBeingDragged =  false;
+            mIsBeingDragged = false;
         } else if (action == MotionEvent.ACTION_UP) {
-            mIsBeingDragged =  false;
+            mIsBeingDragged = false;
         } else if (action == MotionEvent.ACTION_MOVE) {
             final int activePointerId = mActivePointerId;
             final int pointerIndex = ev.findPointerIndex(activePointerId);
@@ -98,15 +89,29 @@ public class MyViewGroup2 extends LinearLayout {
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        int action = event.getAction();
-        Log.w("cjf", "MyViewGroup2#onTouchEvent "+action);
-        if (action == MotionEvent.ACTION_DOWN) {
-//            getParent().requestDisallowInterceptTouchEvent(true);
-        } else if (action == MotionEvent.ACTION_UP) {
-        } else if (action == MotionEvent.ACTION_MOVE) {
-        } else if (action == MotionEvent.ACTION_CANCEL) {
-        }
-        return super.onTouchEvent(event);
+    public boolean onTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        Log.d("cjf", "B#onTouchEvent " + action);
+        return super.onTouchEvent(ev);
     }
+
+
+}
+
+class BB extends LinearLayout {
+
+    public BB(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BB(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        Log.w("cjf", "BB#onTouchEvent " + action);
+        return super.onTouchEvent(ev);
+    }
+
 }
